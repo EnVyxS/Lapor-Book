c52d4ab226db38c9e8ce8927f5bc38b6aafa874a
EnVyxS <mrxn053c54f3@gmail.com>
2023-12-25 15:27:37 +0700

    Upload pertama

---
+++ lapor_book_apps/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:lapor_book_apps/firebase_options.dart';
+import 'package:lapor_book_apps/pages/SplashPage.dart';
+import 'package:lapor_book_apps/pages/LoginPage.dart';
+import 'package:lapor_book_apps/pages/RegisterPage.dart';
+import 'package:lapor_book_apps/pages/DashboardPage.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MaterialApp(
+    title: "Aplikasi Lapor Book",
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const SplashPage(),
+      '/login': (context) => const LoginPage(),
+      '/register': (context) => const RegisterPage(),
+      '/dashboard': (context) => const DashboardPage(),
+      // '/add': (context) => AddFormPage(),
+      // '/detail': (context) => DetailPage(),
+    },
+  ));
+}
